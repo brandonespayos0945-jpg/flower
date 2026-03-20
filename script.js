3d18a2cf4b0092d8be0c91dc32f95a8f307dcee6
brandonespayos0945-jpg <brandonespayos0945@gmail.com>
2026-03-21 01:31:07 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 onload = () => {
   document.body.classList.remove("container");
+  
+    const firefliesContainer = document.querySelector(".fireflies");
+	
+	  // ✨ FIREFLIES
+  for (let i = 0; i < 20; i++) {
+    const f = document.createElement("div");
+    f.classList.add("firefly");
+
+    f.style.left = Math.random() * 100 + "vw";
+    f.style.top = Math.random() * 100 + "vh";
+
+    firefliesContainer.appendChild(f);
+  }
 };
